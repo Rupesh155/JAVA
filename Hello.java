97b841ddcad0e17531452619a56816048f7f9003
Rupesh155 <rudhg35@gmail.com>
2025-04-11 11:09:57 +0530

    java

--- Hello.java
+++ Hello.java
@@ -1,89 +1,147 @@
 
+// class Hello{
+//         public static void main(String[] args) {
+//             Scanner in  =  new   Scanner(System.in);
+//            int a=   in.nextInt();
+//         //    switch (a) {
+//         //        case :
+                   
+//         //            break;
+//         //        default:
+//         //            throw new AssertionError();
+//         //    }
 
-//   class Hello{
-//     public static void main(String[] args) {
-//             //    byte a=5;
-//             //    byte b=5;
-//             //    byte c=a+b
-//             // int  c='c';
-//         //     byte c=129;
-        // System.out.println(c);   
-        // System.out.println(10+10+"hell0");   
 
-//     }
+//         //        Scanner in=     new    Scanner(System.in);
+//         //        System.out.println("Enter your input");
+
+//                 //    int a=     in.nextInt();
+//                 // char b= in.next().charAt(0);
+//                 //    System.out.println(b);
+//                 // int a=5;
+//                 // switch (a) {
+//                 //     case  1:
+//                 //     case  2:
+//                 //     case 3:
+//                 //     case 4:
+//                 //     System.out.println("hello"); 
+//                 //         break;
+//                 //     case 5:
+//                 //     System.out.println("hiiiii");
+//                 //     break;
+//                 //     default:
+//                 //         System.out.println("byeee!!");
+//                 // }
+
+
+
+//                 //  Scanner in=   new   Scanner(System.in);
+//                 // int a=1;
+//                 // while(a<7){
+//                 //         System.out.println("hello");
+//                 //         a++;
+
+
+
+//                 // }
+
+//                 // int a=1;
+//                 // do { 
+//                 //     System.out.println("hello");
+//                 //     a++;
+//                 // } while (a<6);
+//                 // for(int i=0;i<5;i++){
+//                 //         System.out.println(i);
+//                 // }
+//                 //  System.out.println("Enter your 1st value");
+//                 //      int a=     in.nextInt();
+//                 //  System.out.println("Enter your 2st value");
+//                 //  int b=     in.nextInt();
+//                 //  System.out.println("Enter your operatorssss value");
+//                 //  char op=     in.next().charAt(0);
+
+//                 //  switch (op) {
+//                 //      case '+':
+//                 //      System.out.println(a+b);
+//                 //          break;
+//                 // case '-':
+//                 // System.out.println(a-b);
+//                 //      default:
+//                 //          System.out.println("0000000");
+//                 //  }
+//                 //       1 +> "one"
+//                 //       2=> "two"
+
+                 
+//                 //  System.out.println("Enter your no");
+//                 //  char a =in.next().charAt(0);
+//                 //  System.out.println("user value" + a);
+
+//         // Scanner
+                
+//                 // int a=7;
+//                 // switch (a) {
+//                 //     case 1:
+//                 //     case 2:
+//                 //     case 3:
+//                 //     case 4:
+//                 //     System.out.println("hello"); 
+//                 //         break;
+//                 //     case 5:
+//                 //     System.out.println("hiii");
+//                 //     break;
+//                 //     default:
+//                 //         System.out.println("mai hua na last meee");
+//                 // }
+
+                
+//         }
 // }
-// 8
-// 88
-// 888
-//
 
 
 
-public   class Hello{
-    // int a=5;
-   public   static void main(String[] args) {  
-
-    // System.out.println(a);
-    // byte a=5;
-    // System.out.println(a);
-   
-    // int a=5;     00000000 00000000 0000000 0000101         
-    // byte b=a; 00000000
-    // byte a=128    //-128 to 127   000000000
-    // int a=' ';
-    // byte b=(byte)a;
-    // System.out.println(a);
-    // byte c=b+a;
-    // System.out.println(c);
-    // // int a=5;
-//   int  b=127;
-//     byte a =b;
-// byte b=5;
-// short a=b
-// int c=b;
+// class Hello{
+//         public static void main(String[] args) {
+//            Scanner in=  new  Scanner(System.in);
+//         System.out.println("Enter your 1st No");
+//             int a=     in.nextInt();
+//         System.out.println("Enter your 2st No");
+//         int b=     in.nextInt();
+//         System.out.println("Enter you cammanddddddd");
+//         char c=     in.next().charAt(0);
+//         //       in.next()
+//         switch (c) {
+//             case '+':
+//             System.out.println(a+b);
+//                 break;
+//             case '-':
+//             System.out.println(a-b);
+//             default:
+//                System.out.println("kuch bhi hoga aapka");
+//         }
 
 
+//         //  1   => "one"
+//         //  2 => "two"
+  
+//         }
 
-// int a=5;  
-// byte b=a; 0000000000
+// }
+class Hello{
+        public static void main(String[] args) {
+                // for(int i=0;i<5;i++){
+                //         // System.out.println("hello");
+                //         System.out.println(i*2);
 
-                          
-// int a=5;
-// int b=10;
-// int a=5;    
-// byte b=(byte)a; 
-// a = a + b; 
-// b = a - b; 
-// a = a - b;
+                // }
+                int a=2;
+                while(a<5){
+                        System.out.println("kya aayegaa");
+                        a++;
+                }
 
-//  int a=5;
-//  double b=a;
-//  System.out.println(b);
-
-// String name="anu";  
-// int age=20;
-// System.out.println("My name is "+name+" and I am "+age+" years old");
-
-
-        //   128     
-        // System.out.println(false&&true);
-
-        // long a=5;
-        // double b=(int)a;
-        // System.out.println(b);
-        // int a=130;
-        // byte b=(byte)a;
-        // System.out.println(b);
-        //   int a='A'+32+' ';
-        //   System.out.println(a);
-        // boolean a=false;
-        // boolean b=true;
-
-        // System.out.println(ab);
-        // int a =130;
-        // byte b=(byte)a;  256 -128 to 12
-        // System.out.println(b);
-        int a=5;
-        System.out.println(a);
-    }
+          
+        }
 }
+
+
